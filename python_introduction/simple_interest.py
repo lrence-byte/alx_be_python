8a1d1f821209f18a5ab58b28e5932b9dabeaa4e9
lrence-byte <76810347+lrence-byte@users.noreply.github.com>
2024-09-21 12:06:35 +0300

    Simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,15 @@
+principal = 1000
+P = principal
+rate = 0.05
+R = rate 
+time = 3
+T = time 
+
+
+I = interest = principal * rate * time
+
+I = P * R * T
+
+
+print(f"The simple interest is: {I}")
+
